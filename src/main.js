59fc4350c9475cfff5ac298fa70fb90480ca197e
Restyled.io <commits@restyled.io>
2020-09-20 15:07:54 +0000

    Restyled by clang-format

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ var result = extension.then((data) => print(data))
 function print(e) {
   console.log(e)
   var list = '<ul>'
-  for(let i=0; i<e.length; i++)
-  {
-    if(e[i].enabled) {
+  for (let i = 0; i < e.length; i++) {
+    if (e[i].enabled) {
       list += "<li>" + e[i].name + "</li>";
     }
   }
